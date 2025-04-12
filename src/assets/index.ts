51d054c1f56d786872fcc5364e2f69027fc33e26
Evan Katz <ejk37@duke.edu>
2025-04-12 15:01:32 -0400

    loading cells

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,4 +3,3 @@ export {default as Status} from "./status.svelte";
 export {default as Stop} from "./stop.svelte";
 
 export type {Grid, Cell} from "./gridLayout";
-export {gridLayout} from "./gridLayout";
--- src/lib/StorageCell.svelte
+++ src/lib/StorageCell.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Reload from "$assets/reload.svelte";
+
   const {
     id,
     width = 32,
@@ -6,18 +8,27 @@
   }: { id: string; height?: number; width?: number } = $props();
 
   let open = $state(true);
+  let disabled = $state(false);
+  const store = async () => {
+    disabled = true;
+    open = !open;
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    disabled = false;
+  };
 </script>
 
 <button
   id={open ? "open" : "close"}
-  class="flex justify-center items-end bg-green-300 m-1
+  class="flex flex-col justify-end items-center bg-green-300 m-1
         border-gray-300 border rounded-xl shadow-md
         hover:border-[#646cff] focus:border-[#646cff] focus-visible:border-[#646cff]"
   style={`width: ${width}px; height: ${height}px`}
-  onclick={() => {
-    open = !open;
-  }}
+  onclick={store}
+  {disabled}
 >
+  {#if disabled}
+    <Reload width={width / 4} height={height / 4} class="animate-spin" />
+  {/if}
   <p class="text-xs text-gray-600 m-1">{id}</p>
 </button>
 
