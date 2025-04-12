fd1ddc6920e95e7cbb03beac2233eefefe3d0967
Evan Katz <ejk37@duke.edu>
2025-04-11 23:07:53 -0400

    pre tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,68 +1,81 @@
 <script lang="ts">
-	import "./app.css";
-	import svelteLogo from "$assets/store.svg";
-	import Counter from "$components/Counter.svelte";
-	import ReqButton from "$components/ReqButton.svelte";
+  import "./app.css";
+  import svelteLogo from "$assets/store.svg";
+  import ReqButton from "$components/ReqButton.svelte";
+  import { Separator } from "$lib/components/ui/separator";
+  import * as Tabs from "$lib/components/ui/tabs/index.js";
 </script>
 
 <main>
-	<div id="header">
-		
-		<div class="centering">
-			<img src={svelteLogo} class="logo storage" alt="Warehouse Logo" />
-		</div>
-		<h1 id="siteTitle">Storage Control Panel</h1>
-	</div>
+  <div id="header">
+    <div class="centering">
+      <img
+        src={svelteLogo}
+        class="logo storage"
+        alt="Warehouse Logo"
+        id="mainLogo"
+      />
+    </div>
+    <h1 id="siteTitle">Storage Control Panel</h1>
+  </div>
 
-	<div class="btnArray">
-		<div class="card">
-			<Counter />
-		</div>
+  <div class="centering">
+    <Separator class="my-4 w-[75vw]" />
+  </div>
 
-		<div class="card">
-			<ReqButton icon="status" method="GET" uri="http://localhost:5173/" />
-		</div>
-	</div>
+  <div class="centering">
+    <div class="grid max-w-sm sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div class="card">
+        <ReqButton icon="stop" method="GET" uri="http://localhost:5173/" />
+      </div>
+      <div class="card">
+        <ReqButton icon="status" method="GET" uri="http://localhost:5173/" />
+      </div>
+    </div>
+  </div>
 
+  <div class="centering">
+    <Separator class="my-1 w-[25vw]" />
+  </div>
 
-	<div id="footer">
-		<p class="subtext">Powered by Amazon Robotics Team 1</p>
-		<p class="subtext">ME424 Spring 2025</p>
-	</div>
+  <div id="footer">
+    <p class="subtext">Powered by Amazon Robotics Team 1</p>
+    <p class="subtext">ME424 Spring 2025</p>
+  </div>
 </main>
 
 <style>
-	#siteTitle {
-		margin: 4px 0px 0px 0px;
-	}
 
-	.logo {
-		display: block;
-		height: 8em;
-		padding: 0.5em;
-		will-change: filter;
-		transition: filter 300ms;
-	}
-	.logo:hover {
-		filter: drop-shadow(0 0 2em #646cffaa);
-	}
-	.logo.storage:hover {
-		filter: drop-shadow(0 0 2em #ff9900);
-	}
 
-	.subtext {
-		font-size: x-small;
-		margin-block-start: 0em;
-		margin-block-end: 0em;
-	}
-	.btnArray {
-		display: flex;
-		place-content: center;
-		/* display: grid; */
-	}
+  #siteTitle {
+    font-size: 2.5em;
+    line-height: 1.1;
+    margin: 4px 0px 0px 0px;
+  }
 
-	.centering {
-		display: flex;
-		place-content: center;
-	}
+  .card {
+    padding: 1em;
+  }
+
+  #mainLogo {
+    display: block;
+    height: 8em;
+    padding: 0.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  #mainLogo:hover {
+    filter: drop-shadow(0 0 2em #ff9900);
+  }
+
+  .subtext {
+    font-size: x-small;
+    margin-block-start: 0em;
+    margin-block-end: 0em;
+  }
+
+  .centering {
+    display: flex;
+    place-content: center;
+  }
 </style>
--- src/components/ReqButton.svelte
+++ src/components/ReqButton.svelte
@@ -3,19 +3,31 @@
     uri,
     method,
     icon,
-    size = 1,
-  }: { uri: string; method: string; icon: string; size?: number } = $props();
+    raw_size = 1,
+  }: { uri: string; method: string; icon: string; raw_size?: number } = $props();
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { default as Reload } from "$assets/reload.svelte";
+
+  const size = 32 * raw_size
 
   let iconpath;
-  switch(icon) {
-	case "status":
-		iconpath = "/src/assets/status.svelte";
-		break;
-	default:
-		throw "No Icon Found"
+  switch (icon) {
+    case "status":
+      iconpath = "/src/assets/status.svelte";
+      break;
+    case "stop":
+      iconpath = "/src/assets/stop.svelte";
+      break;
+    default:
+      throw "No Icon Found";
   }
 
   const IconComponent = import(/* @vite-ignore */ iconpath);
+  // const ic = async () => {
+  //   await new Promise((resolve) => setTimeout(resolve, 20000));
+  //   return import(/* @vite-ignore */ iconpath);
+  // };
+  // const IconComponent = ic();
 
   const sendReq = async () => {
     try {
@@ -30,20 +42,25 @@
   };
 </script>
 
-<div class="centering">
-  {#await IconComponent then { default: IconComponent }}
-    <button onclick={sendReq} id="reqbutton">
-      <IconComponent width={24 * size} height={24 * size} display="block" />
-    </button>
-  {/await}
-</div>
-
-<style>
-  .centering {
-    display: flex;
-    place-items: center;
-  }
-  #reqbutton {
-    padding: 0.5em 0.5em;
-  }
-</style>
+{#await IconComponent}
+  <Button variant="outline" size="logo" disabled>
+    <Reload
+      class={`p-1 animate-spin`}
+      width={size}
+      height={size}
+    />
+  </Button>
+{:then { default: IconComponent }}
+  <Button
+    variant="outline"
+    size="logo"
+    onclick={sendReq}
+    class="
+    p-1
+	  hover:border-[#646cff] hover:border 
+    focus:border-[#646cff] focus:border 
+		focus-visible:border-[#646cff] focus-visible:border"
+  >
+    <IconComponent width={size} height={size} display="block" />
+  </Button>
+{/await}
